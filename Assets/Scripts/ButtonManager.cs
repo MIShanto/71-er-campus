79ad27d4a73f9ed82fc416346fb571b2e1d64555
MIShanto <shanto4045@gmail.com>
2021-12-23 22:41:03 +0600

    updated

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] Animator my_animator;
     [SerializeField] GameObject mainInfoPanel;
     [SerializeField] GameObject historyPanel;
     [SerializeField] GameObject riddlePanel;
@@ -11,24 +12,33 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] GameObject gotoHistoryButton;
     [SerializeField] GameObject closePanelButton;
 
+    [SerializeField] GameObject[] spotImageHolders;
+
 
     private void OnMouseDown()
     {
-        Debug.LogError(this.gameObject.tag);
 
         if (this.gameObject.CompareTag("Click Btn"))
         {
-            mainInfoPanel.SetActive(true);
+            //mainInfoPanel.SetActive(true);
+            my_animator.Play("open_panel");
+            my_animator.Play("history_panel_swap2");
+
             historyPanel.SetActive(true);
             gotoRiddleButton.SetActive(true);
 
             riddlePanel.SetActive(false);
             gotoHistoryButton.SetActive(false);
             closePanelButton.SetActive(false);
+
+            
         }
         if (this.gameObject.CompareTag("Goto Riddle Btn"))
         {
-            historyPanel.SetActive(false);
+            //historyPanel.SetActive(false);
+            my_animator.Play("history_panel_swap");
+            my_animator.Play("riddle_panel_come");
+
             gotoRiddleButton.SetActive(false);
 
             gotoHistoryButton.SetActive(true);
@@ -37,15 +47,25 @@ public class ButtonManager : MonoBehaviour
         }
         if (this.gameObject.CompareTag("Close Btn"))
         {
+
+            my_animator.Play("riddle_panel_close");
+
+            my_animator.Play("close_panel");
+
             historyPanel.SetActive(false);
             gotoRiddleButton.SetActive(false);
             riddlePanel.SetActive(false);
             closePanelButton.SetActive(false);
-            mainInfoPanel.SetActive(false);
+            //mainInfoPanel.SetActive(false);
+
+            
         }
         if (this.gameObject.CompareTag("Goto History Btn"))
         {
-            historyPanel.SetActive(true);
+            //historyPanel.SetActive(true);
+
+            my_animator.Play("riddle_panel_close");
+            my_animator.Play("history_panel_swap2");
             gotoRiddleButton.SetActive(true);
 
             riddlePanel.SetActive(false);
